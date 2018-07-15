60e1229c9d9249e321f689926b439f6699a58514
Mayank Kumawat <mayankjaiho8@gmail.com>
2018-07-15 12:21:36 -0700

    Content Components css js files

---
+++ src/Components/Content/Content.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './content.css';
+import LoginForm from './../LoginForm/loginForm.js';
+
+const Content = () => {
+
+	return (
+		<div className = "content">
+			<LoginForm />
+		</div>
+		)
+}
+
+export default Content;
